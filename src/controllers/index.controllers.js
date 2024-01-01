d1c307ca838198f4d4532a959e01a68df02d7413
Paul Hidalgo <paulesh5@gmail.com>
2024-01-01 18:41:56 -0500

    Listar portafolios

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,16 +1,16 @@
-
+// Importar el modelo
+const Portfolio = require('../models/Portfolio')
 
 // PRIMERA FUNCION PARA RENDERIZAR EL INDEX
-const renderIndex = (req,res)=>{
-    res.render('index')
+// Metodo para listar todos los portafolios
+const renderIndex = async(req,res)=>{
+    // Consultar todos los portafolios, transformar a JSON y almacenarlos en la variable portfolios
+    const portfolios = await Portfolio.find().lean()
+    // Invocar la vista index y pasar la variable portfolios
+    res.render('index',{portfolios})
 }
 
-// PRIMERA FUNCION PARA RENDERIZAR EL LOGIN
-const renderLogin = (req,res)=>{
-    res.render('login')
-}
-
+// Exportacion de una funcion
 module.exports ={
-    renderIndex, 
-    renderLogin
+    renderIndex
 }
--- src/routers/index.routes.js
+++ src/routers/index.routes.js
@@ -1,7 +1,6 @@
 // IMPORTAR ROUTES DE EXPRESS
 const {Router} = require('express')
 const { renderIndex } = require('../controllers/index.controllers')
-const { renderLogin } = require('../controllers/index.controllers')
 
 // INSTANCIAR ROUTES
 const router = Router()
